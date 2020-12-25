76009e77c0552cdc63980b90f374b4334c553b5d
koji-koji <k.koyama37@gmail.com>
2020-12-25 14:48:37 +0900

    :sparkles: 表示変更用のテストテキストの設定。

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <TestText></TestText>
       </header>
     </div>
   );
---
+++ src/components/parts/TestText.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TestText = () => <div>test</div>;
+
+export default TestText;
