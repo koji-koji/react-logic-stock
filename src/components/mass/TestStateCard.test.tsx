15436cafbbfb600fe77685aea9908ac78bfd48ea
koji-koji <k.koyama37@gmail.com>
2020-12-29 00:00:04 +0900

    :bug: decrementボタンのテストで数値をマイナスするように調整

--- src/components/mass/TestStateCard.test.tsx
+++ src/components/mass/TestStateCard.test.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TestStateCard from './TestStateCard';
 import userEvent from '@testing-library/user-event';
 
 describe('クリックボタンのテスト', () => {
 
-  render(<TestStateCard />);
-  const clickCountElement = screen.getByText('click count', { exact: false })
-  const incrementButton = screen.getByText('increment', { exact: false })
-  const decrementButton = screen.getByText('decrement', { exact: false })
-
   test('click increment button increase click count text', () => {
+    render(<TestStateCard />);
+    const clickCountElement = screen.getByText('click count', { exact: false })
+    const incrementButton = screen.getByText('increment', { exact: false })
     const initialCount: number = parseInt(clickCountElement.textContent!.replace("click count ", ""))
+
     userEvent.click(incrementButton)
     expect(clickCountElement).toHaveTextContent((initialCount + 1).toString())
     userEvent.click(incrementButton)
     expect(clickCountElement).toHaveTextContent((initialCount + 2).toString())
   });
 
-
   test('click decrement button decrease click count text', () => {
+    render(<TestStateCard />);
+    const clickCountElement = screen.getByText('click count', { exact: false })
+    const decrementButton = screen.getByText('decrement', { exact: false })
     const initialCount: number = parseInt(clickCountElement.textContent!.replace("click count ", ""))
+
     userEvent.click(decrementButton)
-    expect(clickCountElement).toHaveTextContent((initialCount).toString())
+    expect(clickCountElement).toHaveTextContent((initialCount - 1).toString())
     userEvent.click(decrementButton)
-    expect(clickCountElement).toHaveTextContent((initialCount).toString())
-    expect(true).toBeTruthy()
+    expect(clickCountElement).toHaveTextContent((initialCount - 2).toString())
   });
 })
-
